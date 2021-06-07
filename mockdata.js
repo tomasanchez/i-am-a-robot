04dd542bbb7be73271c6e7909da5202b14ff942b
Tomas <tosanchez@frba.utn.edu.ar>
2021-06-07 15:02:44 -0300

    refactor: extracted method of randomness

--- mockdata.js
+++ mockdata.js
@@ -7,11 +7,20 @@
  */
 
 /**
+ * Generates a random number between 0 and the current year.
+ *
+ * @returns A random number
+ */
+const randomIdNumber = () => {
+  return Math.floor(Math.random() * (new Date().getFullYear() + 1));
+};
+
+/**
  * Mockdata for Old Reddit.
  */
 const mockdata = {
   password: "p@s$w0rd_(R3)P0leN74",
-  user: `rTest${Math.floor(Math.random() * 999)}Captcha`,
+  user: `rTest${randomIdNumber()}Captcha`,
 };
 
 export default mockdata;
